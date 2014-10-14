4691f13bd994781d93f37bab793cea2b8d507f9e
Connie Yuan <connieyuan92@gmail.com>
2014-10-13 22:25:29 -0700

    Display Indeed XML results to website

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 //Instantiate the app using the Express framework for Node
 var express = require('express');
 var routes = require('./routes/routes.js');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 //Enable static files in /public for stylesheets, fonts, and client side javascript
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+app.use(session({secret: '123456789'}));
 
 app.get('/', routes.get_main);
 app.get('/indeed', routes.get_indeed);
+app.post('/search', routes.post_search);
 
 app.listen(app.get('port'), function() {
    console.log("Node app is running at localhost:" + app.get('port'))
--- routes/routes.js
+++ routes/routes.js
@@ -8,10 +8,36 @@ var getMain = function(req, res) {
 	res.render('main.ejs');
 };
 
-var getIndeed = function(req, res) {
+var getIndeed = function(query, location, callbackToSearch) {
+	var i = 0;
+
+	var indeedQueryPath = "/ads/apisearch?publisher=1474148037537663";
+	if (!query) {
+		callbackToSearch("Please specify a job type", null);
+	} else {
+		// add the search for query into the path
+		indeedQueryPath += "&q=";
+		var queryArray = query.split(" ");
+		for (i = 0; i < queryArray.length; i++) {
+			indeedQueryPath += "+" + queryArray[i];
+		}
+
+		indeedQueryPath += "&l=";
+		if (location) {
+			var locationArray = location.split(" ");
+			for (i = 0; i < locationArray.length; i++) {
+				indeedQueryPath += "+" + locationArray[i];
+			}
+		}
+
+		indeedQueryPath += "&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2";
+		console.log(indeedQueryPath);
+	}
+
+	//'/ads/apisearch?publisher=1474148037537663&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2'
 	var options = {
 		host: 'api.indeed.com',
-		path: '/ads/apisearch?publisher=1474148037537663&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2'
+		path: indeedQueryPath
 	};
 
 	var callback = function(res) {
@@ -24,14 +50,36 @@ var getIndeed = function(req, res) {
 
   		//the whole response has been recieved, so we just print it out here
   		res.on('end', function () {
-    		console.log(str);
+    		callbackToSearch(null, str);
   		});
 	};
 
 	http.request(options, callback).end();
 };
 
-var postSearch = function(req, res) {};
+var postSearch = function(req, res) {
+	var query = req.body.query;
+	var location = req.body.loc;
+	getIndeed(query, location, function(err, data) {
+		if (err) {
+			req.session.message = err;
+			res.redirect('/');
+		} else if (data) {
+			/*
+			var xml2js = require('xml2js');
+			var xml = data;
+
+			var parser = new xml2js.Parser();
+			parser.parseString(xml, function(err,result){
+			  //Extract the value from the data element
+			  console.dir(result);
+			});*/
+		
+			res.render('results.ejs', {data: data});
+			console.log(data);
+		}
+	});
+};
 
 
 var routes = {
